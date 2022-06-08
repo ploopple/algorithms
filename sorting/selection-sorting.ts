5f0bff7de6059adc19620e985c56cb414fed13e4
mohammed alasam <mohammedalasam@mohammeds-Mac-mini.local>
2022-06-08 16:35:30 +0300

    add package.json to the algorithms folder

---
+++ sorting/selection-sorting.ts
@@ -0,0 +1,43 @@
+const selectionSortingMain = (): void => {
+  const arrLength: number = 123;
+  const arrOfNumbersToSort: number[] = [];
+  const isSmallToLarg: boolean = false;
+  const isLargToSmall: boolean = false;
+
+  for (let v = 0; v < arrLength; v++) {
+    arrOfNumbersToSort.push(Math.round(Math.random() * arrLength));
+  }
+
+  const sortingAlgorithm = (arr: number[]): number[] => {
+    const result: number[] = arr;
+
+    for (let j = 0; j < arr.length; j++) {
+      for (let i = j + 1; i < arr.length; i++) {
+        if (isSmallToLarg) {
+          if (result[j] > result[i]) {
+            const temp: number = result[j];
+            result[j] = result[i];
+            result[i] = temp;
+          }
+        } else if (isLargToSmall) {
+          if (result[j] < result[i]) {
+            const temp: number = result[j];
+            result[j] = result[i];
+            result[i] = temp;
+          }
+        } else {
+          return [-1];
+        }
+      }
+    }
+
+    return result;
+  };
+
+  console.log(arrOfNumbersToSort);
+  console.log("-------------------");
+  console.log("-------------------");
+  console.log(sortingAlgorithm(arrOfNumbersToSort));
+};
+
+selectionSortingMain();
