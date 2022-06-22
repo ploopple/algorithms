4e9d62c486ee6d6d821da5f0f827f4a121f8eebf
mohammed alasam <mohammedalasam@mohammeds-Mini.mshome.net>
2022-06-22 08:55:37 +0300

    add the code challenge for the roman rumber coverter

--- src/code-challenges/romanToInt.ts
+++ src/code-challenges/romanToInt.ts
@@ -1,73 +1,151 @@
-interface OBJTYPE {
-  i: {
-    value: number;
-    char: string;
-  };
-  v: {
-    value: number;
-    char: string;
-  };
-  x: {
-    value: number;
-    char: string;
-  };
-  c: {
-    value: number;
-    char: string;
-  };
-  d: {
-    value: number;
-    char: string;
-  };
-  m: {
-    value: number;
-    char: string;
-  };
-}
-
-const obj: any = [
-  { value: 1, char: "I" },
-  { value: 5, char: "V" },
-  { value: 10, char: "X" },
-  { value: 50, char: "L" },
-  { value: 100, char: "C" },
-  { value: 500, char: "D" },
-  { value: 1000, char: "M" },
-];
 function romanToInt(s: string): number {
+  const obj: { value: number; char: string }[] = [
+    { value: 1, char: "I" },
+    { value: 5, char: "V" },
+    { value: 10, char: "X" },
+    { value: 50, char: "L" },
+    { value: 100, char: "C" },
+    { value: 500, char: "D" },
+    { value: 1000, char: "M" },
+  ];
   let result: number = 0;
 
   for (let i: number = 0; i < s.length; i++) {
-    // obj.map((resObj: any) => {
-    //   if (s[i] === resObj.char) {
-    //     console.log(resObj);
-    //     if (s[i + 1]) {
-    //       obj.map((resObj2: any) => {
-    //         if (s[i + 1] === resObj2.char) {
-    //           console.log(resObj2);
-    //         }
-    //       });
-    //     }
-    //     result += resObj.value;
-    //   }
-    // });
-
-    for (let j: number = 0; j < obj.length; j++) {
-      if (s[i] === obj[j].char) {
-        if (s[i + 1] === obj[j].char) {
-          console.log(s[i + 1], obj[j]);
-          result += obj[j].value - 1;
-          i++;
-          break;
+    const index1: number = obj.findIndex((v) => v.char === s[i]);
+    switch (s[i]) {
+      case "I":
+        if (s[i + 1]) {
+          const index2: number = obj.findIndex((v) => v.char === s[i + 1]);
+          if (s[i - 1]) {
+            const index3: number = obj.findIndex((v) => v.char === s[i - 1]);
+            if (obj[index3].value === obj[index1].value) {
+              (result += obj[index3].value), obj[index1].value;
+              break;
+            }
+          }
+          if (obj[index1].value >= obj[index2].value) {
+            result += obj[index1].value;
+          } else {
+            result += obj[index2].value - obj[index1].value;
+            i++;
+          }
+        } else {
+          result += obj[index1].value;
         }
-        // console.log(s[i], obj[j]);
-        result += obj[j].value;
-      }
+        break;
+      case "V":
+        if (s[i + 1]) {
+          const index2: number = obj.findIndex((v) => v.char === s[i + 1]);
+          if (s[i - 1]) {
+            const index3: number = obj.findIndex((v) => v.char === s[i - 1]);
+            if (obj[index3].value === obj[index1].value) {
+              (result += obj[index3].value), obj[index1].value;
+              break;
+            }
+          }
+          if (obj[index1].value >= obj[index2].value) {
+            result += obj[index1].value;
+          } else {
+            result += obj[index2].value - obj[index1].value;
+            i++;
+          }
+        } else {
+          result += obj[index1].value;
+        }
+        break;
+      case "X":
+        if (s[i + 1]) {
+          const index2: number = obj.findIndex((v) => v.char === s[i + 1]);
+          if (s[i - 1]) {
+            const index3: number = obj.findIndex((v) => v.char === s[i - 1]);
+            if (obj[index3].value === obj[index1].value) {
+              (result += obj[index3].value), obj[index1].value;
+              break;
+            }
+          }
+          if (obj[index1].value >= obj[index2].value) {
+            result += obj[index1].value;
+          } else {
+            result += obj[index2].value - obj[index1].value;
+            i++;
+          }
+        } else {
+          result += obj[index1].value;
+        }
+        break;
+      case "L":
+        if (s[i + 1]) {
+          const index2: number = obj.findIndex((v) => v.char === s[i + 1]);
+          if (s[i - 1]) {
+            const index3: number = obj.findIndex((v) => v.char === s[i - 1]);
+            if (obj[index3].value === obj[index1].value) {
+              (result += obj[index3].value), obj[index1].value;
+              break;
+            }
+          }
+          if (obj[index1].value >= obj[index2].value) {
+            result += obj[index1].value;
+          } else {
+            result += obj[index2].value - obj[index1].value;
+            i++;
+          }
+        } else {
+          result += obj[index1].value;
+        }
+        break;
+      //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      case "C":
+        if (s[i + 1]) {
+          const index2: number = obj.findIndex((v) => v.char === s[i + 1]);
+          if (s[i - 1]) {
+            const index3: number = obj.findIndex((v) => v.char === s[i - 1]);
+            if (obj[index3].value === obj[index1].value) {
+              (result += obj[index3].value), obj[index1].value;
+              break;
+            }
+          }
+          if (obj[index1].value >= obj[index2].value) {
+            result += obj[index1].value;
+          } else {
+            result += obj[index2].value - obj[index1].value;
+            i++;
+          }
+        } else {
+          result += obj[index1].value;
+        }
+        break;
+      //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      case "D":
+        if (s[i + 1]) {
+          const index2: number = obj.findIndex((v) => v.char === s[i + 1]);
+          if (s[i - 1]) {
+            const index3: number = obj.findIndex((v) => v.char === s[i - 1]);
+            if (obj[index3].value === obj[index1].value) {
+              (result += obj[index3].value), obj[index1].value;
+              break;
+            }
+          }
+          if (obj[index1].value >= obj[index2].value) {
+            result += obj[index1].value;
+          } else {
+            result += obj[index2].value - obj[index1].value;
+            i++;
+          }
+        } else {
+          result += obj[index1].value;
+        }
+        break;
+      case "M":
+        result += obj[index1].value;
+        break;
     }
   }
-
-  console.log(result);
   return result;
 }
 
-romanToInt("IV");
+console.log(romanToInt("III")); //3
+console.log(romanToInt("LVIII")); //58
+console.log(romanToInt("MCMXCIV")); //1994
+console.log(romanToInt("MDCCCLXXXIV")); //1884
+console.log(romanToInt("MMCCCXCIX")); //2399
+console.log(romanToInt("CXC")); //190
